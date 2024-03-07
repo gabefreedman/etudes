906fa58df79a0a71772ed4772514a3eece3369a4
Gabe Freedman <gfreedman2108@comcast.net>
2024-03-06 20:50:00 -0600

    add base deterministic signal class

---
+++ etudes/constants.py
@@ -0,0 +1,14 @@
+"""
+Useful constants. They're copied from constants.py in `enterprise`.
+"""
+
+import scipy.constants as sc
+
+c = sc.speed_of_light
+pc = sc.parsec
+GMsun = 1.327124400e20
+
+kpc = pc * 1.0e3
+Mpc = pc * 1.0e6
+
+Tsun = GMsun / (c**3)
